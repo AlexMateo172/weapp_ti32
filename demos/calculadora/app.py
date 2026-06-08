c2bb89d246038892393a8f2c91d16126dee7d41a
AlexMateo172 <1725110135@utectulancingo.edu.mx>
2026-06-08 15:01:59 +0000

    CREATED .calculadora

---
+++ demos/calculadora/app.py
@@ -0,0 +1,23 @@
+import web
+
+urls = (
+    '/', 'Index',
+    '/calculadora','Calculadora'
+)
+app = web.application(urls, globals())
+render = web.template.render('templates')
+
+class Index:
+    def GET(self):
+        return render.index()
+    
+class Calculadora:
+    def GET(self):
+        return render.calculadora()
+    
+    def POST(self):
+        formulario = web.input()
+        return f"Formulario: {formulario}"
+
+if __name__ == "__main__":
+    app.run()
