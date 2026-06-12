abc2003fdd0b4b19748664baa5a681c8b2b3a85a
AlexMateo172 <1725110135@utectulancingo.edu.mx>
2026-06-12 01:31:49 +0000

    CREATED .calculadora

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -1,4 +1,5 @@
 import web
+import math #Importa fucniones matematicas como la raiz o la potencia 
 
 urls = (
     '/', 'Index',
@@ -21,12 +22,41 @@ class Calculadora:
     
     def POST(self):
         formulario = web.input()
-        numero_1 = float(formulario['prinum'])
-        numero_2 = float(formulario['segnum'])
-        resultado = numero_1 + numero_2    
-
+                                                         # Uso .get() con 0.0 como valor predeterminado si el campo está vacío
+        numero_1 = float(formulario.get('prinum', 0.0))
+        numero_2 = float(formulario.get('segnum', 0.0))
+        operacion = formulario.get('operacion', '')
         
-       
+        resultado = 0.0
+
+
+        if  operacion == "sumar":
+            resultado = numero_1 + numero_2
+        
+        if  operacion == "restar":
+            resultado = numero_1 - numero_2
+
+        if  operacion == "multiplicar":
+            resultado = numero_1 * numero_2
+
+        if  operacion == "dividir":
+            resultado = numero_1 / numero_2
+
+        if operacion == "raiz":
+            resultado = math.sqrt(numero_1)
+
+        if operacion == "potencia":
+            resultado = math.pow(numero_1, numero_2) #El pow se usa para calcular la potencia de un numero 
+
+        if operacion == "modulo":
+            resultado = numero_1 % numero_2
+
+        if operacion == "limpiar":
+            numero_1 = 0.0
+            numero_2 = 0.0
+            resultado = 0.0                
+
+
         return render.calculadora(numero_1, numero_2, resultado)
 
 if __name__ == "__main__":
